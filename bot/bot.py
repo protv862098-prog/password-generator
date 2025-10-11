8659de1174c9258bd834e5693e45d8c8f7205c5a
protv862098-prog <protv862098@gmail.com>
2025-10-11 19:17:40 +0500

    Create bot.py

---
+++ bot/bot.py
@@ -0,0 +1,68 @@
+import telebot
+from bot_logic import gen_pass
+
+
+# Замени 'TOKEN' на токен твоего бота
+# Этот токен ты получаешь от BotFather, чтобы бот мог работать
+bot = telebot.TeleBot("8473665011:AAEAjIJmNp4-79ielfqwBxarrbQgZzFxBVY")
+
+text_messages = {
+    'welcome':
+        u'Please welcome {name}!\n\n'
+        u'This chat is intended for questions about and discussion of the pyTelegramBotAPI.\n'
+        u'To enable group members to answer your questions fast and accurately, please make sure to study the '
+        u'project\'s documentation (https://github.com/eternnoir/pyTelegramBotAPI/blob/master/README.md) and the '
+        u'examples (https://github.com/eternnoir/pyTelegramBotAPI/tree/master/examples) first.\n\n'
+        u'I hope you enjoy your stay here!',
+
+    'info':
+        u'My name is TeleBot,\n'
+        u'I am a bot that assists these wonderful bot-creating people of this bot library group chat.\n'
+        u'Also, I am still under development. Please improve my functionality by making a pull request! '
+        u'Suggestions are also welcome, just drop them in this group chat!',
+
+    'wrong_chat':
+        u'Hi there!\nThanks for trying me out. However, this bot can only be used in the pyTelegramAPI group chat.\n'
+        u'Join us!\n\n'
+        u'https://telegram.me/joinchat/067e22c60035523fda8f6025ee87e30b'
+}
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Привет! Я твой Telegram бот. Напиши что-нибудь!")
+
+@bot.message_handler(commands=['hello'])
+def send_hello(message):
+    bot.reply_to(message, "Привет! Как дела?")
+
+@bot.message_handler(commands=['bye'])
+def send_bye(message):
+    bot.reply_to(message, "Пока! Удачи!")
+
+
+@bot.message_handler(commands=['heh'])
+def send_heh(message):
+    count_heh = int(message.text.split()[1]) if len(message.text.split()) > 1 else 5
+    bot.reply_to(message, "he" * count_heh)
+
+
+
+
+
+    
+@bot.message_handler(commands=['pass'])
+def send_password(message):
+    password = gen_pass(10)  # Устанавливаем длину пароля, например, 10 символов
+    bot.reply_to(message, f"Вот твой сгенерированный пароль: {password}")
+
+@bot.message_handler(commands=['info'])
+def on_info(message):
+    bot.reply_to(message, text_messages['info'])
+
+
+
+
+
+
+
+bot.polling()
